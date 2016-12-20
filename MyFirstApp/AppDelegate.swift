112b3eda4568385c05919458814339a1df2da2ea
Aviad <aviad31194@gmail.com>
2016-12-20 13:53:44 +0200

    Facebook basic initialization

--- MyFirstApp/AppDelegate.swift
+++ MyFirstApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import Firebase
 import UIKit
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         FIRApp.configure()
         return true
     }
