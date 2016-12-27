f3ee3f3254e5b624e42d2ab73ba0ffa2958569fc
Aviad <aviad31194@gmail.com>
2016-12-27 19:52:59 +0200

    Grocery lists are now queried by the user groups keys. Oh! I wanna dance with somebody Yeah! I wanna dance with somebody With somebody who loves me :((((((

---
+++ MyFirstApp/GroceryListsByGroupDB.swift
@@ -0,0 +1,48 @@
+//
+// Created by admin on 26/12/2016.
+// Copyright (c) 2016 Naveh Ohana. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class GroceryListsByGroupDB {
+    let listsNode = "grocery-lists"
+
+    var query: FIRDatabaseQuery!
+
+    init(groupKey: NSString) {
+        query = FIRDatabase.database().reference(withPath: listsNode)
+                .queryOrdered(byChild: "groupKey").queryEqual(toValue: groupKey)
+    }
+
+    func observeListsAddition(whenAdded: @escaping (_: GroceryList) -> Void) {
+        query.observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            let addedList = self.extractGroceryList(key: snapshot.key as String,
+                    values: snapshot.value as! Dictionary<String, Any>)
+            whenAdded(addedList)
+        })
+    }
+
+    func observeListsDeletion(whenDeleted: @escaping (_: GroceryList) -> Void) {
+        query.observe(FIRDataEventType.childRemoved, with: {(snapshot) in
+            let deletedList = self.extractGroceryList(key: snapshot.key as String,
+                    values: snapshot.value as! Dictionary<String, Any>)
+            whenDeleted(deletedList)
+        })
+    }
+
+    func removeObservers() {
+        query.removeAllObservers()
+    }
+
+    private func extractGroceryList(key: String, values: Dictionary<String, Any>) -> GroceryList {
+        return GroceryList(
+                id: key as NSString,
+                title: values["title"]! as! NSString,
+                date: TimeUtilities.getDateFromString(
+                        date: values["date"]! as! String,
+                        timeZone: TimeZone(secondsFromGMT: 0 - TimeUtilities.getCurrentTimeZoneSecondsFromGMT())!) as NSDate,
+                groupKey: values["groupKey"] as! NSString)
+    }
+}
---
+++ MyFirstApp/UserGroceryListsDB.swift
@@ -0,0 +1,64 @@
+//
+// Created by admin on 27/12/2016.
+// Copyright (c) 2016 Naveh Ohana. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class UserGroceryListsDB {
+    var lists = Array<GroceryList>()
+    var groupsDb: UserGroupsDB?
+    var listsDb = Array<GroceryListsByGroupDB>()
+
+    var whenListAdded: ((_: Int) -> Void)?
+    var whenListDeleted: ((_: Int) -> Void)?
+
+    init(userKey: NSString) {
+        groupsDb = UserGroupsDB(userKey: userKey)
+    }
+
+    func observeLists(whenListAdded: @escaping (_: Int) -> Void, whenListDeleted: @escaping(_: Int) -> Void) {
+        self.whenListAdded = whenListAdded
+        self.whenListDeleted = whenListDeleted
+        groupsDb!.observeUserGroupsAddition(whenGroupAdded: groupAdded)
+    }
+
+    private func groupAdded(groupIndex: Int) {
+        let group = groupsDb!.getGroup(row: groupIndex)
+
+        let db = GroceryListsByGroupDB(groupKey: group!.key)
+        listsDb.append(db)
+
+        db.observeListsAddition(whenAdded: listAdded)
+        db.observeListsDeletion(whenDeleted: listDeleted)
+    }
+
+    private func listAdded(addedList: GroceryList) {
+        lists.append(addedList)
+        whenListAdded!(lists.count - 1)
+    }
+
+    private func listDeleted(deletedList: GroceryList) {
+        let deletedListIndex = lists.index(where: { $0.id == deletedList.id })!
+        lists.remove(at: deletedListIndex)
+        whenListDeleted!(deletedListIndex)
+    }
+
+    func removeObservers() {
+        groupsDb!.removeObservers()
+        listsDb.forEach({ $0.removeObservers() })
+    }
+
+    func getListsCount() -> Int {
+        return lists.count
+    }
+
+    func getGroceryList(row: Int) -> GroceryList? {
+        if (row < getListsCount()) {
+            return lists[row]
+        }
+
+        return nil
+    }
+}
