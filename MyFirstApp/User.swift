92380c9d616779250ae44f3acc3ffcbd6a731bcc
Aviad <aviad31194@gmail.com>
2017-03-14 21:30:15 +0200

    Reverting UserDB changes, there is no point in using local db in one-row queries

--- MyFirstApp/User.swift
+++ MyFirstApp/User.swift
@@ -12,18 +12,10 @@ class User {
     var name:NSString?
     var key:NSString
     var facebookId:NSString?
-    var lastUpdate:NSDate?
 
     init(key: NSString, name: NSString?, facebookId: NSString?) {
         self.key = key
         self.name = name
         self.facebookId = facebookId
-    }
-
-    init(key: NSString, name: NSString?, facebookId: NSString?, lastUpdate: NSDate) {
-        self.key = key
-        self.name = name
-        self.facebookId = facebookId
-        self.lastUpdate = lastUpdate
     }
 }
--- MyFirstApp/UsersDB.swift
+++ MyFirstApp/UsersDB.swift
@@ -13,94 +13,33 @@ class UsersDB {
     static let sharedInstance: UsersDB = { UsersDB() } ()
     let rootNode = "users"
     var databaseRef: FIRDatabaseReference!
-    var localDb: LocalDb!
+    var userCache:Dictionary<String, User> = Dictionary<String, User>()
 
     init() {
         databaseRef = FIRDatabase.database().reference()
-        localDb = LocalDb()!
     }
     
     deinit {
         databaseRef.removeAllObservers()
     }
     
-//    private func manageRefresh() {
-//        // Get the local update time
-//        var localUpdateTime = LastUpdateTable.getLastUpdateDate(database: localDb.database, table: "Users")
-//        
-//        if (localUpdateTime == nil) {
-//            // Init the date with the oldest date
-//            localUpdateTime = Date.init(timeIntervalSince1970: 0)
-//        }
-//        
-//        // Add time interval so that we don't select the same objects
-//        localUpdateTime = localUpdateTime?.addingTimeInterval(1)
-//        
-//        // TODO: Query not working correctly..
-////        findNewUsersFromRemote(localUpdateTime: localUpdateTime!)
-//    }
-    
-//    private func findNewUsersFromRemote(localUpdateTime: Date) {
-//        let nsDate = localUpdateTime as NSDate
-//        databaseRef.child(rootNode).queryOrdered(byChild: "lastUpdated").queryStarting(atValue: nsDate.timeIntervalSince1970).observeSingleEvent(
-//            of: FIRDataEventType.value, with: {(snapshot) in
-//
-//                if !(snapshot.value is NSNull) {
-//                    var latestDate = localUpdateTime
-//                    
-//                    let users = snapshot.value as! Dictionary<NSString, Dictionary<String, Any>>
-//                    
-//                    for child in users.keys {
-//
-//                        let user = self.extractUser(key: child as NSString, values: users[child]! as Dictionary<String, Any>)
-//                        let currentUserDate = user.lastUpdate
-//                        
-//                        // Save in local db
-//                        user.addUserToLocalDb(database: self.localDb.database)
-//                        
-//                        // Save the latest date
-//                        if (currentUserDate?.compare(latestDate) == .orderedDescending) {
-//                            latestDate = currentUserDate as! Date
-//                        }
-//                    }
-//
-//                    print("Updating User LastUpdate to \(latestDate)")
-//                    
-//                    // Update the last update date
-//                    LastUpdateTable.setLastUpdate(database: self.localDb.database, table: "Users", lastUpdate: latestDate)
-//                }
-//        })
-//    }
-    
     func findUserByKey(key: String, whenFinished: @escaping (_: User?) -> Void) {
-//        manageRefresh()
-        
-        // Fetch the user from local db
-//        let userFromLocal = User.getUserByKeyFromLocalDB(database: localDb.database, key: key)
-        
-//        if (userFromLocal == nil) {
-            findUserFromRemote(key: key, whenFinished: whenFinished)
-//        }
-//        else {
-//            whenFinished(userFromLocal)
-//            print("Got user from local.")
-//        }
-    }
-    
-    private func findUserFromRemote(key: String, whenFinished: @escaping (_: User?) -> Void) {
-        databaseRef.child(rootNode).child(key).observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
-            // Make sure the user was found in the database
-            if (!(snapshot.value is NSNull)) {
-                let user = self.extractUser(key: snapshot.key as NSString, values: snapshot.value as! Dictionary<String, Any>)
-                user.addUserToLocalDb(database: self.localDb.database)
-                
-                whenFinished(user)
-                print("Got user from remote.")
-            }
-            else {
-                whenFinished(nil)
-            }
-        })
+        if (self.userCache[key] == nil) {
+            databaseRef.child(rootNode).child(key).observeSingleEvent(of: FIRDataEventType.value, with: { (snapshot) in
+                    // Make sure the user was found in the database
+                    if (!(snapshot.value is NSNull)) {
+                        let user = self.extractUser(key: snapshot.key as NSString, values: snapshot.value as! Dictionary<String, Any>)
+                        self.userCache[key] = user
+                        
+                        whenFinished(user)
+                    } else {
+                        whenFinished(nil)
+                    }
+            })
+        }
+        else {
+            whenFinished(self.userCache[key]!)
+        }
     }
 
     func findUserByFacebookId(facebookId: String, whenFinished: @escaping (_: User) -> Void) {
@@ -115,30 +54,19 @@ class UsersDB {
     }
     
     private func extractUser(key: NSString, values: Dictionary<String, Any>) -> User {
-        return User(
-                key: key,
-                name: values["name"] as? NSString,
-                facebookId: values["facebookId"] as? NSString,
-                lastUpdate: TimeUtilities.getDateFromString(
-                        date: values["lastUpdated"]! as! String,
-                        timeZone: TimeZone(secondsFromGMT: 0 - TimeUtilities.getCurrentTimeZoneSecondsFromGMT())!) as NSDate)
+        return User(key: key, name: values["name"] as? NSString, facebookId: values["facebookId"] as? NSString)
     }
     
     func addUser(user:User, whenFinished: @escaping (Error?, FIRDatabaseReference) -> Void) {
         let values = loadValues(from: user)
-        
-        // Add to remote
         self.databaseRef.child(rootNode).child(user.key as String).setValue(values, withCompletionBlock: whenFinished)
-        
-        // Add to local db
-        user.addUserToLocalDb(database: localDb.database)
+        self.userCache[user.key as String] = user
     }
     
-    private func loadValues(from: User) -> Dictionary<String, Any> {
-        var values = Dictionary<String, Any>()
+    private func loadValues(from: User) -> Dictionary<String, String> {
+        var values = Dictionary<String, String>()
         values["name"] = from.name as? String
         values["facebookId"] = from.facebookId as? String
-        values["lastUpdated"] = FIRServerValue.timestamp()
         
         return values
     }
